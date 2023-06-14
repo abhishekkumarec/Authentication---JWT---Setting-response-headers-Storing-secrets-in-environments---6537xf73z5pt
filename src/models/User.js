256d5ab821653d137de994c9d24cd9a7cd1078df
Abhishek Kumar <126902053+abhishekkumarec@users.noreply.github.com>
2023-06-14 10:30:48 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,21 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  // Your code goes here
-})
-
-const User = mongoose.model('user',userSchema);
-
-module.exports = User;
+  first_name: {
+    type:String,
+    required:true
+  },
+  last_name: {
+    type:String,
+    required:true
+  },
+  email: {
+    type:String,
+    required:true,
+  },
+  password: {
+    type:String,
+    required:true
+  }
+});
+module.exports = mongoose.model("user", userSchema);
