a8dea11d97d59e17b32e8967726e52a4871d70e0
Abhishek Kumar <126902053+abhishekkumarec@users.noreply.github.com>
2023-06-14 10:48:43 +0530

    Update RefreshToken.js

--- src/models/RefreshToken.js
+++ src/models/RefreshToken.js
@@ -1,9 +1,1 @@
-const mongooose = require('mongoose');
 
-const tokenSchema = new mongooose.Schema({
-    // Your code goes here
-})
-
-const RefreshToken = mongooose.model('tokens', tokenSchema);
-
-module.exports = RefreshToken;
